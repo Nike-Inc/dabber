473aa1eb08927c40fc54177ef2028e4ae8aabdc8
Tim Kye <Timothy.Kye@nike.com>
2017-10-18 14:16:25 -0700

    add backup readout

--- src/dynamo.js
+++ src/dynamo.js
@@ -9,14 +9,18 @@ module.exports = {
 }
 
 function backup (options) {
+  let prefix = `${options.s3Prefix}/${(options.datestamp || new Date().toISOString())}/${options.dbTable}`
   let backup = new Backup({
     S3Bucket: options.s3Bucket,
-    S3Prefix: `${options.s3Prefix}/${(options.datestamp || new Date().toISOString())}/${options.dbTable}`,
+    S3Prefix: prefix,
     S3Region: options.s3Region,
     DbTable: options.dbTable,
     DbRegion: options.dbRegion || options.s3Region
   })
   return backup.full()
+    .then(() => {
+      console.log('backup complete', options.s3Bucket, prefix)
+    })
 }
 
 function restore (options) {
